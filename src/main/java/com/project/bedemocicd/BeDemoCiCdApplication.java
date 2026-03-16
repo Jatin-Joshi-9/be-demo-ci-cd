3a965ac08a7c1b85ee71997c34f92954fe523443
Jatin-Joshi-9 <jatinjoshi090803@gmail.com>
2026-03-16 13:08:50 +0530

    push initial setup

---
+++ src/main/java/com/project/bedemocicd/BeDemoCiCdApplication.java
@@ -0,0 +1,13 @@
+package com.project.bedemocicd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeDemoCiCdApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BeDemoCiCdApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/project/bedemocicd/BeDemoCiCdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.bedemocicd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeDemoCiCdApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
